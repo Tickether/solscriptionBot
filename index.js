d152deaef2277730bdf2e9a200f6cdc561853333
Tickether <tickether@gmail.com>
2023-01-26 22:26:09 +0000

    base work
    verify token discoed bot

--- index.js
+++ index.js
@@ -4,26 +4,25 @@ const ethers = require('ethers'); // import ethers
 const fs = require('fs');
 const axios = require('axios');
 const Database = require('@replit/database');
+
+
+
 const db = new Database();
-
-
 
 const client = new Discord.Client(); //create new client
 
-
-
 // Replace YOUR_PROVIDER with the URL of a JSON-RPC provider
-const provider = new ethers.providers.JsonRpcProvider('https://mainnet.infura.io/v3/8231230ce0b44ec29c8682c1e47319f9');
+const provider = new ethers.providers.JsonRpcProvider('https://goerli.infura.io/v3/8231230ce0b44ec29c8682c1e47319f9');
 
 // Replace YOUR_CONTRACT_ADDRESS with the address of the contract
-const solscriptionAddress = '0xE0Be388Ab81c47B0f098D2030a1c9Ef190691a8A';
+const solscriptionAddress = '0x611Ea02425A83Ab6018e7149166ECf2E48D8F0CA';
 
 // Replace YOUR_ABI_FILE with the path to the JSON file containing the ABI
 const abiFile = './Solscription.json';
 
 const solscriptionAbi = JSON.parse(fs.readFileSync(abiFile, 'utf8'));
 
-const solscriptionContract = new ethers.Contract(solscriptionAddress, solscriptionAbi.output.abi, provider);
+const solscriptionContract = new ethers.Contract(solscriptionAddress, solscriptionAbi.abi, provider);
 
 const axiosInstance = axios.create({
     headers: {
@@ -79,12 +78,14 @@ async function listKeys(){
 
 
 
-// fetch token owned async
+// fetch holder list and check if wallet is in true of false
 async function fetchWallet(address) {
     
     try {
-        const solsWallet = await axios.get('https://api.etherscan.io/api?module=account&action=tokennfttx&contractaddress='+solscriptionAddress+'&address='+address+'&page=1&offset=100&startblock=0&endblock=27025780&sort=asc&apikey=S3KASSMNT3ARZHEUU2NM9G3IMXH98BB8W7');
-        return solsWallet.data.result[0]['to']
+        let solsWallet  = await axios.get('https://api-goerli.etherscan.io/api?module=account&action=tokennfttx&contractaddress='+solscriptionAddress+'&address='+address+'&page=1&offset=100&startblock=0&endblock=27025780&sort=asc&apikey=S3KASSMNT3ARZHEUU2NM9G3IMXH98BB8W7');
+        solsWallet = solsWallet.data.result
+        return solsWallet
+       
     } catch(err) {
         console.log(err);
     }
@@ -94,7 +95,7 @@ async function fetchWallet(address) {
 async function fetchToken(address) {
     
     try {
-        const solsTokenOwned = await axios.get('https://api.etherscan.io/api?module=account&action=tokennfttx&contractaddress='+solscriptionAddress+'&address='+address+'&page=1&offset=100&startblock=0&endblock=27025780&sort=asc&apikey=S3KASSMNT3ARZHEUU2NM9G3IMXH98BB8W7');
+        const solsTokenOwned = await axios.get('https://api-goerli.etherscan.io/api?module=account&action=tokennfttx&contractaddress='+solscriptionAddress+'&address='+address+'&page=1&offset=100&startblock=0&endblock=27025780&sort=asc&apikey=S3KASSMNT3ARZHEUU2NM9G3IMXH98BB8W7');
         return solsTokenOwned.data.result[0]['tokenID']
     } catch(err) {
         console.log(err);
@@ -111,21 +112,20 @@ async function fetchOpenseaBio(address) {
         console.log(err);
     }
 }
-
+/*
 // check opensea bio for added code
-async function openseaBioChange(code, openseaBio) {
+async function openseaBioChange(address, code) {
     
     try {
         // if code added to bio (included in bio) assign role 
         if (openseaBio.includes(code)) {
             return true                      
-        } else {
-            return false
         }
     } catch(err) {
         console.log(err);
     }
 }
+*/
 
 
 
@@ -133,31 +133,33 @@ async function openseaBioChange(code, openseaBio) {
 
 client.on('ready', async () => {
     client.channels.cache.get("946417287672496145").send("Please enter the wallet you want to verify subscription for to begin");
-    let keys = await listKeys();
-    if (keys === !null ) {
-        for (let i = 0; i < keys.length; i++) {
-            // get key value iwth address token and expiry
-            let keyValue = await getKeyValue(keys[i])
-            let timeStamp = new Date().getUTCMilliseconds();
-            if (keyValue[2] < timeStamp) {
-                // query actual token expiry 
-                let userExpires = await solscriptionContract.ownerOf(solsTokenOwned)
-                console.log(userExpires);
-                // if not renewd
-                if (userExpires < timeStamp ) {
-                    //remove role
-                    const guild =  message.guild;
-                    const role = guild.roles.cache.get('1063121737769820270');
-                    const member = guild.members.cache.get(message.author.id);
-                    member.roles.remove(role);
-                    await deleteKey();
+    async function getKeysDeleteRoles() {
+        let keys = await listKeys();
+        if (keys === !null ) {
+            for (let i = 0; i < keys.length; i++) {
+                // get key value iwth address token and expiry
+                let keyValue = await getKeyValue(keys[i])
+                let timeStamp = new Date().getUTCMilliseconds();
+                if (keyValue[2] < timeStamp) {
+                    // query actual token expiry 
+                    let userExpires = await solscriptionContract.ownerOf(solsTokenOwned)
+                    console.log(userExpires);
+                    // if not renewd
+                    if (userExpires < timeStamp ) {
+                        //remove role
+                        const guild =  message.guild;
+                        const role = guild.roles.cache.get('1063121737769820270');
+                        const member = guild.members.cache.get(message.author.id);
+                        member.roles.remove(role);
+                        await deleteKey(keys[i]);
+                    }
                 }
             }
+        } else {
+            console.log('no users wallets info')
         }
-    } else {
-        console.log('no users wallets info')
     }
-    
+    setInterval(getKeysDeleteRoles, 300000)
     // login consol message
     console.log(`Logged in as ${client.user.tag}!`);
 });
@@ -174,54 +176,67 @@ client.on("message", async message => {
             // get sols wallet from async above
             let solsWallet = await fetchWallet(address)
             console.log(solsWallet)
-            // verify input wallet is a Sols Wallet
-            if (solsWallet.toLowerCase() === address.toLowerCase()) {
-                // get tokens owned from async above
-                let solsTokenOwned = await fetchToken(address)
-                console.log(solsTokenOwned)
-                // check user is on active sub
-                let userOf = await solscriptionContract.userOf(solsTokenOwned)
-                console.log(userOf);
-                // wallet set in subs must be equal to owner of token //if sub not set return a zero address
-                if (userOf.toLowerCase() === solsWallet.toLowerCase()) {
-                    
-                    // check expiry of sub
-                    let userExpires = await solscriptionContract.userExpires(solsTokenOwned)
-                    console.log(userExpires);
-                    
-                    // Generate code
-                    const code = Math.random().toString(36).substring(7);
-                    
-                    // Send message with code to the user
-                    message.reply(`Your Wallet ownes subscription token [${solsTokenOwned}] expring on [${userExpires}] Enter code [${code}] into your Opensea Bio to confirm your OWnership and get discord role`);
-                    
-                    // opensea bio get & check for code included in bio
-                    const openseaBio = await fetchOpenseaBio(address);
-                    console.log(openseaBio);
-                    
-                    // if code not added setIterval check code
-                    let nIntervId;
-                    if (!nIntervId) {   
-                        nIntervId = setInterval(openseaBioChange, 3000)
-                        if (openseaBioChange(code, openseaBio)) {
+            // check if wallet query has membership token or not
+            if (solsWallet.length >= 1) {
+                solsWallet = solsWallet[0].to
+                console.log(solsWallet)
+
+                // verify input wallet is a Sols Wallet
+                if (solsWallet === address.toLowerCase()) {
+
+                    // get tokens owned from async above
+                    let solsTokenOwned = await fetchToken(address)
+                    console.log(solsTokenOwned)
+
+                    // check user is on active sub
+                    let userOf = await solscriptionContract.userOf(solsTokenOwned)
+                    console.log(userOf);
+
+                    // wallet set in subs must be equal to owner of token //if sub not set return a zero address
+                    if (userOf.toLowerCase() === address.toLowerCase()) {
+                        
+                        // check expiry of sub
+                        let userExpires = await solscriptionContract.userExpires(solsTokenOwned)
+                        userExpires = parseInt(userExpires['_hex'],16)
+                        console.log(userExpires);
+                        
+                        // Generate code
+                        const code = Math.random().toString(36).substring(7);
+                        
+                        // Send message with code to the user
+                        await message.reply(`Your Wallet ownes subscription token [${solsTokenOwned}] expring on [${new Date(userExpires * 1000)}] Enter code [${code}] into your Opensea Bio to confirm your OWnership and get discord role`);
+                        
+                        
+                        
+                        //  setIterval check code
+                        nIntervId = setInterval(async () => {
+                            // code to check the database goes here
+                            // opensea bio get & check for code included in bio
+                            const openseaBio = await fetchOpenseaBio(address);
+                            console.log(openseaBio);
+                        }, 15000)
+
+                        if (openseaBio.includes(code)) {
                             clearInterval(nIntervId);
-                            nIntervId = null;
                             const guild =  message.guild;
-                            const role = guild.roles.cache.get('1063121737769820270');
+                            const role = guild.roles.cache.get('1068292965606375425');
                             const member = guild.members.cache.get(message.author.id);
                             member.roles.add(role);
-                            await setKey(message.author.id, [userOf, solsTokenOwned, userExpires])
+                            //await setKey(message.author.id, [userOf, solsTokenOwned, userExpires])
                             
                             // Send message with code to the user
-                            message.reply(`Your discord role assigned you can view all locked channels`);
-                        } 
-                    }
-                } else {
-                    message.reply("subscription not active, please activate it & try again")
+                            message.reply(`Your discord role assigned you can view all locked channels unlocked by the role`);
+                        }
+                        
+                        //console.log(nIntervId)
+                        
+            
+                    } else {
+                        message.reply("subscription not active, please activate it & try again")
+                    }                    
                 }
             } else { 
-                message.reply("you dont have a subscription token, please enter another valid Ethereum address")
-                console.log("err");
+                await message.reply("you dont have a subscription token, please enter another valid Ethereum address")                                
             }
         } else {
             message.reply("please enter a valid Ethereum address")
